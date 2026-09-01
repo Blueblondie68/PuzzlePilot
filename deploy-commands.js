646be1a6c2744aa8a7f6f901c40a5ec5594cf955
Blueblondie68 <sue68wilson@gmail.com>
2026-09-01 20:53:35 +0100

    Update slash commands and use environment token

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
 const commands = [
     new SlashCommandBuilder()
-        .setName('daily')
-        .setDescription('Open the daily game menu')
+        .setName('crossword-leaderboard')
+        .setDescription('Show today’s crossword leaderboard'),
+
+    new SlashCommandBuilder()
+        .setName('crossword-stats')
+        .setDescription('Show your crossword stats'),
 ].map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken("placeholder");
-
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 rest.put(
     Routes.applicationCommands("1540439382157172847"),
@@ -15,4 +19,3 @@ rest.put(
 )
 .then(() => console.log("Slash commands registered."))
 .catch(console.error);
-
